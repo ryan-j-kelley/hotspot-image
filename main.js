99c71a45ebbccb946f1632578e3cee44b3374120
Ryan Kelley <ryankelley@Ryans-MacBook-Pro.local>
2017-03-27 09:19:47 -0400

    Final edits

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert("Hello, World!");
+// alert("Hello, World!");
 $(document).ready(function () { // we must wait for the DOM to be ready as the browser can take a little while to read all of the HTML and build the elements
 
 	$('.circle').on('click', function (e) {
